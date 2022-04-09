614034cec6252915476eb51c9f43fc0817485cef
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-09 20:14:37 +0200

    initial setup completed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const connectDB = require('./config/db.config')
+const connect = require('./config/db.config')
 
-connectDB()
+connect()
 const app = express()
 
 app.use(cors())
