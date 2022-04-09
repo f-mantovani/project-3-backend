7867066689a5d2fe76c90abf34b0ce90d98ceff2
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-09 21:06:56 +0200

    Use and Book models basic - done

--- models/User.model.js
+++ models/User.model.js
@@ -1,3 +1,24 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require("mongoose");
 
-module.exports = model('User', userSchema)
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+  events: [{ type: Schema.Types.ObjectId, ref: "Event" }],
+  books: [{ type: Schema.Types.ObjectId, ref: "Book" }],
+})
+
+module.exports = model("User", userSchema)
