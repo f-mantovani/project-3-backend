63c0ca8500bd6efef222945c3181e1ab92e7a26b
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-09 19:29:51 +0200

    db.config done

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const connectDB = require('./config/db.config')
 
-
+connectDB()
 const app = express()
 
 app.use(cors())
