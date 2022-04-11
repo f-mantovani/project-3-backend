fc66c7353885df2d6d7a85e264dcf53ac55255ed
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-11 18:30:23 +0200

    put-route: create a task

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,15 +1,26 @@
 const { Router } = require('express')
 
 const Task = require('../models/Tasks.model.js')
+const User = require('../models/User.model.js')
 
 const router = Router()
 
 router.post('/', async (req, res) => {
+  const { title } = req.body
+
+  const { userId } = req.user
+
   try {
-    console.log("teste")
-  } catch (error) {
-    console.log(error)
+
+    const newTask = await Task.create({ title, user: userId })
     
+    const user = await User.findByIdAndUpdate(userId, { $push: { tasks: newTask._id }})
+    
+    res.status(201).json(newTask)
+  
+  } catch (error) {
+    
+    res.status(500).json({ place:"error trying create new task", error: error.message })
   }
 })
 
