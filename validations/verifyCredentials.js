f62d377385ba878ae51b8e373dd1ea37d3f290c1
lucas-emg <lucas.gabriel3.lg@gmail.com>
2022-04-11 09:06:21 -0300

    small line adjustmentment for readability

--- validations/verifyCredentials.js
+++ validations/verifyCredentials.js
@@ -1,11 +1,19 @@
 const verifyCredentials = (credential, status, message) => {
+
   if(credential) {
+
     const error = new Error
+
     error.status = status
+
     error.message = message
+
     throw error
+
   }
+
   return
+  
 }
 
 module.exports = { verifyCredentials }
