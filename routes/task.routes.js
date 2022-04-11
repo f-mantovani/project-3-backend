36bd0aad4d09bbd370acdc5138b33a41ef20f457
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-11 18:54:08 +0200

    get-route: get all tasks

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -20,7 +20,48 @@ router.post('/', async (req, res) => {
   
   } catch (error) {
     
-    res.status(500).json({ place:"error trying create new task", error: error.message })
+    res.status(500).json({ place:"Error trying create new task", error: error.message })
+  }
+})
+
+router.get('/', async (req, res) => {
+  const { userId } = req.user
+
+  try {
+    const allTasks = await Task.find({user: userId})
+
+    res.status(200).json(allTasks)
+
+  } catch (error) {
+
+    res.status(500).json({ place: "Error trying to get all tasks", error: error.message })
+  }
+})
+
+router.put('/:taskId', async (req, res) => {
+  const { userId } = req.user
+  const { taskId } = req.params
+  try {
+    
+  } catch (error) {
+    
+  }
+})
+router.delete('/', async (req, res) => {
+  const { userId } = req.user
+  try {
+    
+  } catch (error) {
+    
+  }
+})
+router.delete('/:taskId', async (req, res) => {
+  const { userId } = req.user
+  const { taskId } = req.params
+  try {
+    
+  } catch (error) {
+    
   }
 })
 
