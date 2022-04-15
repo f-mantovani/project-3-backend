beae0f5542ff3e08faa911a8e4e4f07af61badc8
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-15 02:47:03 +0200

    the page appears in the web

---
+++ routes/documentation.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+
+  res.sendFile(`${__dirname}/documentation/index.html `)
+ 
+})
+
+module.exports = router
