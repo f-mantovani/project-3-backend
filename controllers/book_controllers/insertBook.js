24c83c2831442663dc91fb96efe87019c950cdea
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-05-06 16:39:11 +0200

    update to changeBookshelves

--- controllers/book_controllers/insertBook.js
+++ controllers/book_controllers/insertBook.js
@@ -1,5 +1,9 @@
-const insertBook = (user, bookId,status) => {
-
+const insertBook = (user, bookId, status) => {
+  const verifyBook = user[status].map((book) => book._id.toString()).indexOf(bookId)
+  if (verifyBook === -1) {
+    user[status].push(bookId)
+  }
+  return user
 }
 
 module.exports = insertBook
--- controllers/user_controllers/changeBookshelves.js
+++ controllers/user_controllers/changeBookshelves.js
@@ -1,8 +1,10 @@
 const changeBookshelves = (user, bookId, status, oldStatus ) => {
-  if (oldStatus !== undefined) {
-    const userMinusOld = user[oldStatus].filter((book) => book._id.toString() !== bookId)
-    user[oldStatus] = userMinusOld
-  }
+  const userRemoveToRead = user.booksToRead.filter((book) => book._id.toString() !== bookId)
+  user.booksToRead = userRemoveToRead
+  const userRemoveReading = user.booksReading.filter((book) => book._id.toString() !== bookId)
+  user.booksReading = userRemoveReading
+  const userRemoveDone = user.booksDone.filter((book) => book._id.toString() !== bookId)
+  user.booksDone = userRemoveDone
   const verifyBook = user[status].map((book) => book._id.toString()).indexOf(bookId)
   if (verifyBook === -1) {
     user[status].push(bookId)
