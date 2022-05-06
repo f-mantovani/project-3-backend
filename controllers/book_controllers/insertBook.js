407592741d348d91a765de7b84a8ad52c26f1f6b
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-05-06 16:10:46 +0200

    updates to changingBookshelves

---
+++ controllers/book_controllers/insertBook.js
@@ -0,0 +1,5 @@
+const insertBook = (user, bookId,status) => {
+
+}
+
+module.exports = insertBook
--- controllers/user_controllers/changeBookshelves.js
+++ controllers/user_controllers/changeBookshelves.js
@@ -1,13 +1,8 @@
 const changeBookshelves = (user, bookId, status, oldStatus ) => {
-  if (user[oldStatus].filter((book) => book._id.toString() !== bookId) === undefined) {
-    const verifyBook = user[status].map((book) => book._id.toString()).indexOf(bookId)
-    if (verifyBook === -1) {
-      user[status].push(bookId)
-    }
-    return user
+  if (oldStatus !== undefined) {
+    const userMinusOld = user[oldStatus].filter((book) => book._id.toString() !== bookId)
+    user[oldStatus] = userMinusOld
   }
-  const userMinusOld = user[oldStatus].filter((book) => book._id.toString() !== bookId)
-  user[oldStatus] = userMinusOld
   const verifyBook = user[status].map((book) => book._id.toString()).indexOf(bookId)
   if (verifyBook === -1) {
     user[status].push(bookId)
