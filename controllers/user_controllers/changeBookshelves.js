1b66bb2f0bce33a1ba7e6a6ecf2c9b7c9ba72e9c
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-05-06 15:40:32 +0200

    updating changing bookshelves

--- controllers/user_controllers/changeBookshelves.js
+++ controllers/user_controllers/changeBookshelves.js
@@ -1,7 +1,13 @@
 const changeBookshelves = (user, bookId, status, oldStatus ) => {
+  if (user[oldStatus].filter((book) => book._id.toString() !== bookId) === undefined) {
+    const verifyBook = user[status].map((book) => book._id.toString()).indexOf(bookId)
+    if (verifyBook === -1) {
+      user[status].push(bookId)
+    }
+    return user
+  }
   const userMinusOld = user[oldStatus].filter((book) => book._id.toString() !== bookId)
   user[oldStatus] = userMinusOld
-  console.log(user)
   const verifyBook = user[status].map((book) => book._id.toString()).indexOf(bookId)
   if (verifyBook === -1) {
     user[status].push(bookId)
