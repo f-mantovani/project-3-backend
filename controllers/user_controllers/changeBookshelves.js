a47eab340df94765d95853451a2c7bce3e9ef749
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-05-06 05:35:03 +0200

    adding to a list removes from another

--- controllers/user_controllers/changeBookshelves.js
+++ controllers/user_controllers/changeBookshelves.js
@@ -1,6 +1,7 @@
 const changeBookshelves = (user, bookId, status, oldStatus ) => {
-  const userMinusOld = user[oldStatus].filter((book) => book.toString() !== bookId)
+  const userMinusOld = user[oldStatus].filter((book) => book._id.toString() !== bookId)
   user[oldStatus] = userMinusOld
+  console.log(user)
   const verifyBook = user[status].map((book) => book._id.toString()).indexOf(bookId)
   if (verifyBook === -1) {
     user[status].push(bookId)
