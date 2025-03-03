393204cdeb8b8477e9dba34e33d05a79d701ca64
f-mantovnai <92172133+f-mantovani@users.noreply.github.com>
2025-03-03 17:30:08 +0100

    update to use the node:path

--- routes/documentation.routes.js
+++ routes/documentation.routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express')
-
+const path = require('node:path')
 const router = Router()
 
 router.get('/', async (req, res) => {
 
-  res.sendFile(`${__dirname}/documentation/index.html`)
+  res.sendFile(path.join(__dirname, 'configuration', 'index.html'))
 
 })
 
