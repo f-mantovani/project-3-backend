132f6b8047ed5f503974e362301455eb6cf0d298
f-mantovnai <92172133+f-mantovani@users.noreply.github.com>
2025-03-03 17:35:40 +0100

    update path resolver

--- routes/documentation.routes.js
+++ routes/documentation.routes.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', async (req, res) => {
 
-  res.sendFile(path.join(__dirname, 'configuration', 'index.html'))
+  res.sendFile(path.reolve(__dirname, '../documentation/index.html'))
 
 })
 
